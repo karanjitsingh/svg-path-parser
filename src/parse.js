498d2262ed46c7b71a1bcba288bd2da503e5d641
Karan Jit Singh <karan1866@gmail.com>
2017-05-11 12:16:38 +0530

    setup test page

--- src/parse.js
+++ src/parse.js
@@ -41,26 +41,26 @@ var parser = {
 				case 'M':
 					x = path.getNextPoint() + rx;
 					y = path.getNextPoint() + ry;
-					console.log("Move to ", x, y);
+					ctx.moveTo(x, y);
 					break;
 				case 'l':
 				case 'L':
 					var x1 = path.getNextPoint() + rx;
 					var y1 = path.getNextPoint() + ry;
-					console.log("Draw line ", x, y, x1, y1);
+					ctx.lineTo(x1, y1);
 					x = x1;
 					y = y1;
 					break;
 				case 'h':
 				case 'H':
 					var x1 = path.getNextPoint() + rx;
-					console.log("Draw line ", x, y, x1, y);
+					ctx.lineTo(x1, y);
 					x = x1;
 					break;
 				case 'v':
 				case 'V':
 					var y1 = path.getNextPoint() + ry;
-					console.log("Draw line ", x, y, x, y1);
+					ctx.lineTo(x, y1);
 					y = y1;
 					break;
 				case 'z':
@@ -79,19 +79,3 @@ var parser = {
 		ctx.closePath();
 	}
 };
-var ctx = {
-	save: function () {
-		console.log("Saved canvas state.");
-	},
-	beginPath: function () {
-		console.log("Beginning canvas path.");
-	},
-	restore: function () {
-		console.log("Restoring canvas state.");
-	},
-	closePath: function () {
-		console.log("Closing canvas path.");
-	}
-};
-
-parser.doMagic(ctx, "M 10 10 H 90 100 V 90");
