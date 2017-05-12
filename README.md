09b6437008ac50fd952b6d7e16c7a5367fff89e3
Karan Jit Singh <karan1866@gmail.com>
2017-05-12 18:24:26 +0530

    added Endpoint to Center Arc parameterization

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # svg-path-parser
 
 A simple parser that parses SVG path data and draws it on canvas. This is essentially to support SVG paths in [particle.js](https://github.com/karanjitsingh/particle.js).
+
+## Notes
+Mapping ```move```, ```line```, ```cubic-bezier``` and ```quadratic-bezier``` from svg to canvas was fairly simple as there is direct support for these commands in ```CanvasRenderingContext2D``` interface.
+
+Mapping ```elliptical-arc``` command was however a bit more complex. There is no support for elliptical arcs in the interface. I decided to draw the elliptical arc as a series of bezier curves.
+
+## References
+* [Rendering an SVG elliptical arc as bezier curves](https://mortoray.com/2017/02/16/rendering-an-svg-elliptical-arc-as-bezier-curves/)
+* [Drawing an elliptical arc using polylines, quadratic
+or cubic Bezier curves](http://www.spaceroots.org/documents/ellipse/elliptical-arc.pdf) - L. Maisonobe
--- src/parse.js
+++ src/parse.js
@@ -11,9 +11,75 @@ var parser = {
 
 		path = path.split(',').join(' ').split('\n').join(' ').split(' ');
 
-		var getNextPoint = function () {
-			return parseFloat(path.shift());
-		};
+		function getNextPoint() { return parseFloat(path.shift()); }
+		function getNextFlag() { return parseInt(path.shift()); }
+		function drawEllipticalArc(rx, ry, phi, flagA, flagS, x1, y1, x2, y2) {
+
+			function clamp(value, min, max) {
+				Math.min(Math.max(value, min), max)
+			}
+
+			function svgAngle(ux, uy, vx, vy ) {
+				var dot = ux*vx + uy*vy;
+				var len = Math.sqrt(ux*ux + uy*uy) * Math.sqrt(vx*vx + vy*vy);
+
+
+
+				var ang = Math.acos( clamp(dot / len,-1,1) );
+				if ( (ux*vy - uy*vx) < 0)
+					ang = -ang;
+				return ang;
+			}
+
+			var rX = Math.abs(rx);
+			var rY = Math.abs(ry);
+
+			var dx2 = (x1 - x2)/2;
+			var dy2 = (y1 - y2)/2;
+
+			var x1p =  Math.cos(phi)*dx2 + Math.sin(phi)*dy2;
+			var y1p = -Math.sin(phi)*dx2 + Math.cos(phi)*dy2;
+
+			var rxs = rX * rX;
+			var rys = rY * rY;
+			var x1ps = x1p * x1p;
+			var y1ps = y1p * y1p;
+
+			var cr = x1ps/rxs + y1ps/rys;
+			if (cr > 1) {
+				var s = Math.sqrt(cr);
+				rX = s * rX;
+				rY = s * rY;
+				rxs = rX * rX;
+				rys = rY * rY;
+			}
+
+			var dq = (rxs * y1ps + rys * x1ps);
+			var pq = (rxs*rys - dq) / dq;
+			var q = Math.sqrt( Math.max(0,pq) );
+			if (flagA === flagS)
+				q = -q;
+			var cxp = q * rX * y1p / rY;
+			var cyp = - q * rY * x1p / rX;
+
+			var cx = Math.cos(phi)*cxp - Math.sin(phi)*cyp + (x1 + x2)/2;
+			var cy = Math.sin(phi)*cxp + Math.cos(phi)*cyp + (y1 + y2)/2;
+
+			var theta = svgAngle( 1,0, (x1p-cxp) / rX, (y1p - cyp)/rY );
+
+			var delta = svgAngle(
+				(x1p - cxp)/rX, (y1p - cyp)/rY,
+				(-x1p - cxp)/rX, (-y1p-cyp)/rY);
+
+			delta = delta - Math.PI * 2 * Math.floor(delta / (Math.PI * 2));
+
+			if (!flagS)
+				delta -= 2 * Math.PI;
+
+			// r_ = float2((float)rX,(float)rY);
+			// c = float2((float)cx,(float)cy);
+			// angles = float2((float)theta, (float)delta);
+		}
 
 		var command = '', symbol, prevCommand = '';
 
@@ -32,7 +98,7 @@ var parser = {
 			else
 				path.unshift(symbol);
 
-			var rx, ry, x1, y1, cpx1, cpx2, cpy1, cpy2, m, c, tx2, ty2;
+			var rx, ry, x1, y1, cpx1, cpx2, cpy1, cpy2, m, c, fa, fs, phi;
 			rx = ry = 0;
 			if (command.toLowerCase() === command) {
 				rx = x;
@@ -144,7 +210,18 @@ var parser = {
 					break;
 				case 'a':
 				case 'A':
-					ctx.ar
+					cpx1 = getNextPoint();
+					cpy1 = getNextPoint();
+					phi = getNextPoint();
+					fa = getNextFlag();
+					fs = getNextFlag();
+					x1 = getNextPoint() + rx;
+					y1 = getNextPoint() + ry;
+
+					drawEllipticalArc(cpx1, cpy1, phi, fa, fs, x, y, x1, y1);
+					
+					ctx.x = x1;
+					y = y1;
 					break;
 				case 'z':
 				case 'Z':
